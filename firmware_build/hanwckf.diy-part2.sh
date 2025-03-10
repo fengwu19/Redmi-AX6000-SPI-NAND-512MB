91b74806f3f05e7a45812d88537c557206e933b7
fengwu19 <zhlongfei03@gmail.com>
2025-03-10 18:49:36 +0800

    Update hanwckf.diy-part2.sh

--- firmware_build/hanwckf.diy-part2.sh
+++ firmware_build/hanwckf.diy-part2.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+!/bin/bash
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+ Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+
+ This is free software, licensed under the MIT License.
+ See /LICENSE for more information.
+
+ https://github.com/P3TERX/Actions-OpenWrt
+ File name: diy-part2.sh
+ Description: OpenWrt DIY script part 2 (After Update feeds)
+
+
+ Modify default IP
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 ##-----------------Add OpenClash dev core------------------
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
